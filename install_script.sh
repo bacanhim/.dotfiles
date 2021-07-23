e959e2a1e50cd1081c8edc6ba80067250450cfbf
Hélder Bacanhim <6317993-bacanhim@users.noreply.gitlab.com>
2021-07-23 23:24:53 +0000

    cloning dotfiles over https since cant clone over ssh due a known hosts error

--- install_script.sh
+++ install_script.sh
@@ -86,7 +86,7 @@ arch-chroot /mnt echo "bacanhim ALL=(ALL) NOPASSWD:ALL" >> /mnt/etc/sudoers
 arch-chroot /mnt runuser -l bacanhim -c "cd /tmp && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm"
 arch-chroot /mnt runuser -l bacanhim -c 'yay -S polybar snapper-gui-git ttf-unifont consolas-font noto-fonts-main betterlockscreen-git spotify --noconfirm'
 echo "DOWNLOADING AND APPLYING DOTFILES"
-arch-chroot /mnt runuser -l bacanhim -c "cd /home/bacanhim/ && git clone git@gitlab.com:bacanhim/.dotfiles.git"
+arch-chroot /mnt runuser -l bacanhim -c "cd /home/bacanhim/ && git clone https://gitlab.com/bacanhim/.dotfiles.git"
 arch-chroot /mnt runuser -l bacanhim -c 'cd /home/bacanhim/.dotfiles && stow --target="$HOME" --no-folding .'
 arch-chroot /mnt sed -i "s\bacanhim ALL=(ALL) NOPASSWD:ALL\ \g" /etc/sudoers
 arch-chroot /mnt sed -i "s\load-module module-role-cork\#load-module module-role-cork\g" /etc/pulse/default.pa
