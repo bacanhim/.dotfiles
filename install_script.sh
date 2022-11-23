71a2f8ec47d08c4e8d7fc7a0ee8043ac51a9f3d1
Hélder Bacanhim <43825339+bacanhim@users.noreply.github.com>
2022-11-23 17:05:12 +0000

    added notifications via xfce4-notifyd and fix grub

--- install_script.sh
+++ install_script.sh
@@ -61,16 +61,16 @@ echo "BACANHIM PASSWORD"
 arch-chroot /mnt passwd bacanhim
 arch-chroot /mnt echo "bacanhim ALL=(ALL) NOPASSWD:ALL" >>/mnt/etc/sudoers
 arch-chroot /mnt runuser -l bacanhim -c 'ssh-keygen -t ed25519 -C "Gitlab"'
-arch-chroot /mnt pacman -S mpv pacman-contrib polybar python-dbus arandr ntfs-3g gvfs nfs-utils ntp unzip tar duf zip htop packagekit acpi acpi_call tlp acpid sddm polkit-gnome xorg xorg-server nvidia nvidia-utils nvidia-settings alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack blueman playerctl flameshot bspwm sxhkd rofi alacritty ranger neofetch stow thunar feh firefox teamspeak3 discord capitaine-cursors ttf-cascadia-code ttf-fira-code noto-fonts materia-gtk-theme papirus-icon-theme grub-theme-vimix --noconfirm
+arch-chroot /mnt pacman -S mpv pacman-contrib xfce4-notifyd polybar python-dbus arandr ntfs-3g gvfs nfs-utils ntp unzip tar duf zip htop packagekit acpi acpi_call tlp acpid sddm polkit-gnome xorg xorg-server nvidia nvidia-utils nvidia-settings alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack blueman playerctl flameshot bspwm sxhkd rofi alacritty ranger neofetch stow thunar feh firefox teamspeak3 discord capitaine-cursors ttf-cascadia-code ttf-fira-code noto-fonts materia-gtk-theme papirus-icon-theme grub-theme-vimix --noconfirm
 arch-chroot /mnt runuser -l bacanhim -c "cd /tmp && git clone https://aur.archlinux.org/paru.git && cd paru && makepkg -si --noconfirm"
-arch-chroot /mnt runuser -l bacanhim -c 'paru -S chili-sddm-theme betterlockscreen-git timeshift timeshift-autosnap spotify-tui oh-my-zsh-git zsh-theme-powerlevel10k-git zsh-syntax-highlighting-git zsh-autosuggestions-git --noconfirm'
+arch-chroot /mnt runuser -l bacanhim -c 'paru -S visual-studio-code-bin chili-sddm-theme betterlockscreen-git timeshift timeshift-autosnap spotify-tui oh-my-zsh-git zsh-theme-powerlevel10k-git zsh-syntax-highlighting-git zsh-autosuggestions-git ttf-font-awesome-5 --noconfirm'
 echo "DOWNLOADING AND APPLYING DOTFILES"
 arch-chroot /mnt runuser -l bacanhim -c "cd /home/bacanhim/ && git clone https://github.com/bacanhim/.dotfiles.git"
 arch-chroot /mnt runuser -l bacanhim -c 'cd /home/bacanhim/.dotfiles && stow --target="$HOME" --no-folding .'
-arch-chroot /mnt cp -R /usr/share/grub/themes/* /boot/grub/themes/
-arch-chroot /mnt echo 'GRUB_THEME="/boot/grub/themes/Vimix/theme.txt"' >>/etc/default/grub
+arch-chroot /mnt runuser -l root cp -R /usr/share/grub/themes/* /boot/grub/themes/
+arch-chroot /mnt sed -i 's\#GRUB_THEME="/path/to/gfxtheme"\GRUB_THEME="/boot/grub/themes/Vimix/theme.txt"\g' /etc/default/grub
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
-arch-chroot /mnt sed -i 's\Current=\Current=chili\g' /etc/sddm.conf.d/kde_settings.conf
+# arch-chroot /mnt sed -i 's\Current=\Current=chili\g' /etc/sddm.conf.d/kde_settings.conf
 arch-chroot /mnt sed -i "s\bacanhim ALL=(ALL) NOPASSWD:ALL\ \g" /etc/sudoers
 arch-chroot /mnt systemctl enable sddm.service
 arch-chroot /mnt systemctl enable NetworkManager
