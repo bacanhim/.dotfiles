ea6813bb70d32fc64d257731090c87fa4522a7a7
Hélder Bacanhim <43825339+bacanhim@users.noreply.github.com>
2022-12-28 21:12:26 +0000

    a

--- bspwm.sh
+++ bspwm.sh
@@ -2,7 +2,7 @@
 sudo su
 pacman -S gnome-keyring libsecret libgnome-keyring picom mpv pacman-contrib xfce4-notifyd polybar python-dbus arandr spotify-launcher ntfs-3g gvfs nfs-utils ntp unzip tar duf zip htop packagekit acpi acpi_call tlp acpid sddm polkit-gnome xorg xorg-server nvidia nvidia-utils nvidia-settings alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack blueman playerctl flameshot bspwm sxhkd rofi alacritty ranger neofetch stow thunar feh firefox teamspeak3 discord capitaine-cursors ttf-cascadia-code ttf-fira-code noto-fonts materia-gtk-theme papirus-icon-theme grub-theme-vimix --noconfirm
 cd /tmp && git clone https://aur.archlinux.org/paru-bin.git && cd paru-bin && makepkg -si --noconfirm
-paru -S auto-cpufreq visual-studio-code-bin chili-sddm-theme betterlockscreen-git timeshift-bin timeshift-autosnap oh-my-zsh-git zsh-theme-powerlevel10k-git zsh-syntax-highlighting-git zsh-autosuggestions-git ttf-font-awesome-5 --noconfirm
+paru -S auto-cpufreq visual-studio-code-bin chili-sddm-theme betterlockscreen-git oh-my-zsh-git zsh-theme-powerlevel10k-git zsh-syntax-highlighting-git zsh-autosuggestions-git ttf-font-awesome-5 --noconfirm
 echo "DOWNLOADING AND APPLYING DOTFILES"
 cd /home/bacanhim/ && git clone https://github.com/bacanhim/.dotfiles.git
 cd /home/bacanhim/.dotfiles && stow --target="$HOME" --no-folding .
@@ -12,7 +12,6 @@ grub-mkconfig -o /boot/grub/grub.cfg
 sed -i 's\Current=\Current=chili\g' /usr/lib/sddm/sddm.conf.d/default.conf
 sed -i "s\bacanhim ALL=(ALL) NOPASSWD:ALL\ \g" /etc/sudoers
 systemctl enable sddm.service
-systemctl enable NetworkManager
 systemctl enable bluetooth
 systemctl enable reflector.timer
 systemctl enable fstrim.timer
--- snapper.sh
+++ snapper.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+pacman -S snapper snap-pac
 umount /.snapshots/
 rm -rf /.snapshots/
 snapper -c root create-config /
-sed -i 's\ALLOW_USERS=""\ALLOW_USERS="bacanhim"\g' /etc/snapper/configs/root
+sed -i 's\ALLOW_GROUPS=""\ALLOW_GROUPS="wheel"\g' /etc/snapper/configs/root
 chmod a+rx /.snapshots/
 systemctl enable --now snapper-timeline.timer
 systemctl enable --now snapper-cleanup.timer
 systemctl enable --now grub-btrfs.path
+snapper -c root create -d "*** BASE SYS CONFIG  ***"
